c9babc7169ac1b7d50e15c7c43e1423dfd538581
Nargiza Naamatbekova <naamatbeknargiza@gmail.com>
2024-12-09 12:36:24 +0600

    create project hb

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>heppy Birthday to you,</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/8QF9hM1MQwc?si=nKAlR2glexixBKv_"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+}
